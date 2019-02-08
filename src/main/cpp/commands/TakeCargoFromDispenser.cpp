c2678590e27aff09f533e3f9890f5ceaff34bbeb
PhyXTGears-1720 <phyxtgearspro@gmail.com>
2019-02-07 22:25:25 -0500

    Document goal and command sequence for TakeCargoFromDispenser
    STATUS: Builds.  Not tested.
    
    - Justin C

--- src/main/cpp/commands/TakeCargoFromDispenser.cpp
+++ src/main/cpp/commands/TakeCargoFromDispenser.cpp
@@ -9,6 +9,29 @@
 #include "Robot.h"
 #include "RobotMap.h"
 
+/* GOAL:
+ *
+ * Capture cargo from dispenser.
+ * 
+ *
+ * Command start:
+ *  - Rotate cargo intake to dispenser position.
+ *  - Retract cargo kick completely (use time delay of ? seconds) before
+ *    engaging rollers. It should already be retracted, but expect the unexpected.
+ *  - Turn on rollers for intake.
+ * 
+ * Command finished when:
+ *  - Cargo detected.  
+ * 
+ * Command end:
+ *  - Stop rollers.
+ *  - Retract cargo kick.
+ * 
+ * Follow-up options:
+ *  - Schedule next command to position cargo intake for scoring. Use
+ *    CommandGroup.
+ */
+
 TakeCargoFromDispenser::TakeCargoFromDispenser() {
     // This command needs the drivetrain subsystem to be available while running.
     Requires(&Robot::m_CargoIntake);
