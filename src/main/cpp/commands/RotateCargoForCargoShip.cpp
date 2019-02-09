7cff5fc6c7b43d3400ebcfd1dd44e672dc518aff
PhyXTGears <phyxtgearspro@gmail.com>
2019-02-09 18:52:56 -0500

    Implement command RotateCargoForCargoShip
    STATUS: Builds. Not tested.
    
    - Tim Z

--- src/main/cpp/commands/RotateCargoForCargoShip.cpp
+++ src/main/cpp/commands/RotateCargoForCargoShip.cpp
@@ -25,14 +25,21 @@ RotateCargoForCargoShip::RotateCargoForCargoShip() {
     Requires(&Robot::m_CargoIntake);
 }
 
-void RotateCargoForCargoShip::Initialize() {}
+void RotateCargoForCargoShip::Initialize() {
+    Robot::m_CargoIntake.RotateToPosition("cargo_ship_shoot_angle");
+}
 
 void RotateCargoForCargoShip::Execute() {}
 
-bool RotateCargoForCargoShip::IsFinished() {}
+bool RotateCargoForCargoShip::IsFinished() {
+    return Robot::m_CargoIntake.IsRotationDone();
+}
 
 void RotateCargoForCargoShip::End() {
     // Make sure the motors stop moving when they aren't being controlled.
+    Robot::m_CargoIntake.StopRotation();
 }
 
-void RotateCargoForCargoShip::Interrupted() {}
+void RotateCargoForCargoShip::Interrupted() {
+    Robot::m_CargoIntake.StopRotation();
+}
