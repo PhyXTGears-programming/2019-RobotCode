e41695938092e7db406b3a178894f51f3adce546
PhyXTGears-1720 <phyxtgearspro@gmail.com>
2019-02-09 11:45:39 -0500

    Fix indentation and remove copyright notice.
    STATUS: Just comments and indentation changed.
    
    - Logan H-D

--- src/main/include/commands/TeleOpDrive.h
+++ src/main/include/commands/TeleOpDrive.h
@@ -1,20 +1,13 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 #pragma once
 
 #include <frc/commands/Command.h>
 
 class TeleOpDrive : public frc::Command {
- public:
-  TeleOpDrive();
-  void Initialize() override;
-  void Execute() override;
-  bool IsFinished() override;
-  void End() override;
-  void Interrupted() override;
+    public:
+        TeleOpDrive();
+        void Initialize() override;
+        void Execute() override;
+        bool IsFinished() override;
+        void End() override;
+        void Interrupted() override;
 };
