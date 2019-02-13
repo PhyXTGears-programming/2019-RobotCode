a0b4ea60f5776845a82d96cf95f165dfc13de974
PhyXTGears <phyxtgearspro@gmail.com>
2019-02-12 20:54:22 -0500

    Implement command RotateHatchForFloor.
    STATUS: Builds. Not tested.
    
    - Joshuah H

--- src/main/cpp/commands/RotateHatchForFloor.cpp
+++ src/main/cpp/commands/RotateHatchForFloor.cpp
@@ -23,16 +23,22 @@ RotateHatchForFloor::RotateHatchForFloor() {
 }
 
 // Called just before this Command runs the first time
-void RotateHatchForFloor::Initialize() {}
+void RotateHatchForFloor::Initialize() {
+    Robot::m_CargoIntake.RotateToPosition("hatch-floor-pickup-angle");
+}
 
 // Called repeatedly when this Command is scheduled to run
 void RotateHatchForFloor::Execute() {}
 
 // Make this return true when this Command no longer needs to run execute()
-bool RotateHatchForFloor::IsFinished() { return false; }
+bool RotateHatchForFloor::IsFinished() {
+    return Robot::m_CargoIntake.IsRotationDone();
+}
 
 // Called once after isFinished returns true
-void RotateHatchForFloor::End() {}
+void RotateHatchForFloor::End() {
+    Robot::m_CargoIntake.StopRotation();
+}
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
